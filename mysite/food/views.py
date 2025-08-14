20ddbd6e60094f1898820b175fa5260d1d0cfdf4
unknown <mahrishisinghrathore@gmail.com>
2025-08-14 17:25:29 +0530

    updating the food app

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world")
+
+def item(request):
+    return HttpResponse("This is an item view")
+
+def foot(request):
+    return HttpResponse("<h1>This is an foot view</h1>") #can also pass the the html
