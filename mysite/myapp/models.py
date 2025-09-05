4629b91126d43a8a2eb7a978ce7e42b8aaad027c
unknown <mahrishisinghrathore@gmail.com>
2025-09-05 20:56:35 +0530

    inserting initial model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    item_name=models.CharField(max_length=200)
+    desc=models.CharField(max_length=633)
+    price=models.IntegerField()
