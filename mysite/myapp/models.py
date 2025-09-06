79e93542e2e3b9aadacb0fc54faa386001c8994a
unknown <mahrishisinghrathore@gmail.com>
2025-09-06 10:36:12 +0530

    learing python shell and model manager concepts

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -6,3 +6,6 @@ class Item(models.Model):
     item_name=models.CharField(max_length=200)
     desc=models.CharField(max_length=633)
     price=models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
+from .models import Item
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hehehhehe")
+    items=Item.objects.all()
+    return HttpResponse(items)
 
 def ReviewAPI(request):
     return HttpResponse("Review Called")
