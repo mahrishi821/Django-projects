b1713dea7ad049f356bda8c5d229db88946d9a1a
unknown <mahrishisinghrathore@gmail.com>
2025-09-06 12:34:13 +0530

    implemented name spacing in the django urls and templates

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index,ReviewAPI,testview
+from .views import index,ReviewAPI,testview,detailview
+app_name="myapp"
 urlpatterns = [
-    path('index/',index,name="index"),
+    path('',index,name="index"),
     path('review/',ReviewAPI,name='review'),
-    path('testview/',testview.as_view(),name='test_view')
+    path('testview/',testview.as_view(),name='test_view'),
+    path('<int:id>/',detailview,name="detail_view"),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,12 +5,20 @@ from .models import Item
 # Create your views here.
 
 def index(request):
-    items=Item.objects.all()
+    items=Item.objects.all() # for queryset
     context={
         "item_list":items
     }
     # return HttpResponse(request,"myapp/index.html")
     return render(request,"myapp/index.html",context)
+
+def detailview(request,id):
+    item=Item.objects.get(id=id) # for an single item we can use this
+    context={
+        "item":item
+    }
+    return render(request,"myapp/detail.html",context)
+    # return HttpResponse(f"Detail view for {item}")
 
 def ReviewAPI(request):
     return HttpResponse("Review Called")
