98d83ad975bd81c5e78a084fc8df238cb3e8383a
unknown <mahrishisinghrathore@gmail.com>
2025-09-15 20:01:39 +0530

    CURD and forms completed

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index,ReviewAPI,testview,detailview,create_item
+from .views import index,ReviewAPI,testview,detailview,create_item, update_item,delete_item
 app_name="myapp"
 urlpatterns = [
     path('',index,name="index"),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('testview/',testview.as_view(),name='test_view'),
     path('<int:id>/',detailview,name="detail_view"),
     path('add/',create_item,name="create_item"),
+    path('update/<int:id>/',update_item,name="update_item"),
+    path('delete/<int:id>/',delete_item,name="delete_item"),
 ]
