bb86875f00fbdb3c159b2b00f6475083486c8fc1
unknown <mahrishisinghrathore@gmail.com>
2025-09-18 00:37:16 +0530

    login functionality in the project

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import register
 urlpatterns = [
     path('register/',register,name="register"),
+    path('login/',auth_views.LoginView.as_view(template_name="users/login.html"),name='login')
 ]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForms
 # Create your views here.
 
 def register(request):
-    form=UserCreationForm()
+    form=RegisterForms()
     if request.method=="POST":
-        form= UserCreationForm(request.POST)
+        form= RegisterForms(request.POST)
 
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
             messages.success(request,f"Welcome {username}, your account has been created")
 
-            return redirect("myapp:index")
+            return redirect("login")
 
 
     return render(request,'users/register.html',{"form":form})
