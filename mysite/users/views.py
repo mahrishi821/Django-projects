6fcfaf058004d59bb2395f02d7338cacd145d4e5
unknown <mahrishisinghrathore@gmail.com>
2025-09-17 23:46:41 +0530

    working on the learning the authentication registration with forms

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -6,11 +6,11 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
 
     if request.method=="POST":
-        form = UserCreationForm(request.POST)
+        form= UserCreationForm(request.POST)
 
         if form.is_valid():
             form.save()
             return redirect("myapp:index")
 
 
-    return render(request,'users/register.html',{'form':form})
+    return render(request,'users/register.html')
